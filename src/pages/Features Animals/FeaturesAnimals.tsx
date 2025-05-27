0c48d69eaf6fc7eb16e9f08401cbd9e4506a5164
Kaluna7 <kaluna12345678910@gmail.com>
2025-05-27 15:34:22 +0800

    Import the GridViewFeatures to FeaturesAnimals for the gridview

--- src/pages/Features Animals/FeaturesAnimals.tsx
+++ src/pages/Features Animals/FeaturesAnimals.tsx
@@ -1,13 +1,26 @@
+import GridViewFeatures from "../../components/layout/GridViewFeatures";
+import FeaturesImg from "../../assets/images/5.jpg";
 
 
 
 export default function FeaturesAnimals() {
+
+  function handleClick(value : string){
+    console.log(value);
+  }
+
   return (
     <div className="h-screen">
      <div className="flex flex-row">
       <h1 className="text-[60px]">Featured Animals</h1>
       <div className="">
-        
+        <GridViewFeatures 
+        img={FeaturesImg}
+        label="test"
+        description="Halo Halo"
+        btnName="Test123"
+        onChange={() => handleClick}
+        />
       </div>
      </div>
         
